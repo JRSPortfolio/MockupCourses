bb904212093614fd34d124e3535b37ff78c3e85d
João <uselessregistry@gmail.com>
2023-01-25 19:53:31 +0000

    "Exercicio"

--- web/hello_fastapi.py
+++ web/hello_fastapi.py
@@ -14,5 +14,10 @@ async def read_root():
 async def read_item(item_id: int, q: str | None = None):
     return {"2xitem_id": item_id * 2, "q": q}
 
+#Exercicio
+@app.get("/soma/{x},{y}")
+async def Sum(x: int, y: int):
+    return f"X + Y = {x + y}"
 
+ 
 # Por omissão FastAPI gera JSON

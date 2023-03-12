f72ec329a4d36c25c325da5032bc393686895b10
João <uselessregistry@gmail.com>
2023-03-12 18:36:48 +0000

    get and post initialization from project

--- proj/D - Users and Forms/views/account.py
+++ proj/D - Users and Forms/views/account.py
@@ -1,7 +1,9 @@
 from datetime import date
 from fastapi_chameleon import template #type: ignore
-from fastapi import APIRouter
-from common import base_viewmodel_with
+from fastapi import APIRouter, Request
+from common import (base_viewmodel_with, 
+                    is_valid_name,
+                    form_field_as_str)
 from data.models import Student
 
 router = APIRouter()
@@ -27,8 +29,33 @@ async def register():
     return register_viewmodel()
 
 def register_viewmodel():
-    return base_viewmodel_with({'error': False,
-                                'error_msg': 'There was an error with your data. Please try again.'})
+    return base_viewmodel_with({'name' : '',
+                                'email' : '',
+                                'password' : '',
+                                'birth_date' : '',
+                                'min_date' : '1800-01-01',
+                                'max_date' : date.today(),
+                                'checked' : False})
+
+@router.post('/account/register')
+@template(template_file = 'account/register.pt')
+async def post_register(request: Request):
+    return post_register_viewmodel(request)
+
+async def post_register_viewmodel(request: Request):
+    form_data = await request.form()
+    name = form_field_as_str(form_data, 'name')
+    email = form_data['email']
+    
+    if not is_valid_name(name):
+        error, error_msg = True, 'Invalid Name!'
+    else:
+        error, error_msg = False, ''
+    return base_viewmodel_with({'error' : error,
+                                'error_msg' : error_msg,
+                                'name' : name})
+        
+         
 
 @router.get('/account/login')
 @template()
